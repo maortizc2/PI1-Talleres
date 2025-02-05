04c67744d026a574685ddaa2d57de15a7b6e429d
maortizc2 <maortizc2@eafit.edu.co>
2025-02-05 11:15:00 -0500

    cambios a la funcion home y al gitignore

--- movie/views.py
+++ movie/views.py
@@ -6,7 +6,7 @@ def home(request):
     #return HttpResponse("Welcome")
     #Se cambia porque con render proviene de un html y el httpresponse es un texto que se ingresa aqui mismo
     #return render(request, 'home.html')
-    return render(request, 'home.html', {'name': 'Greg Lim'})
+    return render(request, 'home.html', {'name': 'Maria Alejandra Ortiz'})
 
 def about(request):
     return HttpResponse("About Us")
