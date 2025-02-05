afc0e9984cb827fe2d2da18f4618897128f348ff
maortizc2 <maortizc2@eafit.edu.co>
2025-02-05 16:47:32 -0500

    visualizacion de peliculas con estilo

--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -20,11 +20,12 @@ from movie import views as movieViews
 
 from django.conf.urls.static import static
 from django.conf import settings
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', movieViews.home),
-    path('about/', movieViews.about),
+    path('about/', views.about, name='about'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
